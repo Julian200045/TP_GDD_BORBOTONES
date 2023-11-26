199ed751c1fb3e14607dbc5310d2a13d14836460
MarcosGurru <mgurruchaga@frba.utn.edu.ar>
2023-11-26 17:45:59 -0300

    avances

--- script_creacion_BI.sql
+++ script_creacion_BI.sql
@@ -98,29 +98,31 @@ BEGIN
 	)
 
 	ALTER TABLE LOS_BORBOTONES.BI_TH_Anuncio
-	ADD CONSTRAINT FK_Tiempo FOREIGN KEY (anuncio_anio_publicacion, anuncio_cuatrimestre_publicacion, anuncio_mes_publicacion) 
+	ADD CONSTRAINT FK_Tiempo_Anuncio FOREIGN KEY (anuncio_anio_publicacion, anuncio_cuatrimestre_publicacion, anuncio_mes_publicacion) 
 	REFERENCES LOS_BORBOTONES.BI_D_TIEMPO(tiempo_anio, tiempo_cuatri, tiempo_mes);
 
 
 	--CREACION TABLA DE HECHOS DE LOS ALQUILERES
-	/*CREATE TABLE LOS_BORBOTONES.BI_TH_Alquiler(
+	CREATE TABLE LOS_BORBOTONES.BI_TH_Alquiler(
 		alquiler_rango_etario_inquilino numeric(18,0) FOREIGN KEY REFERENCES LOS_BORBOTONES.BI_D_RangoEtario(rangoEtario_codigo),
+		alquiler_barrio numeric(18,0) FOREIGN KEY REFERENCES LOS_BORBOTONES.BI_D_Barrio(barrio_codigo),
 		alquiler_anio_inicio numeric(18,0),
 		alquiler_cuatrimestre_inicio numeric(18,0),
 		alquiler_mes_inicio numeric(18,0),
-		alquiler_cantidad_periodos numeric(18,0),
 		alquiler_comision numeric(18,0),
-		alquiler_barrio numeric(18,0) FOREIGN KEY REFERENCES LOS_BORBOTONES.BI_D_Barrio(barrio_codigo),
+	
 	   -- alquiler_porcentaje_incumplimiento numeric(18,2) , DUDOSO
 	   -- alquiler_porcentaje_promedio_incremento numeric(18,2), DUDOSO
 
-		PRIMARY KEY (alquiler_rango_etario_inquilino,alquiler_anio_inicio,alquiler_cuatrimestre_inicio)
+		PRIMARY KEY (alquiler_rango_etario_inquilino,alquiler_barrio,alquiler_anio_inicio,
+		alquiler_cuatrimestre_inicio, alquiler_mes_inicio)
 	)
 
 	ALTER TABLE LOS_BORBOTONES.BI_TH_Alquiler
-	ADD CONSTRAINT FK_Tiempo FOREIGN KEY (alquiler_anio_inicio, alquiler_cuatrimestre_inicio, alquiler_mes_publicacion) 
+	ADD CONSTRAINT FK_Tiempo_Alquiler FOREIGN KEY (alquiler_anio_inicio, alquiler_cuatrimestre_inicio, alquiler_mes_inicio) 
 	REFERENCES LOS_BORBOTONES.BI_D_TIEMPO(tiempo_anio, tiempo_cuatri, tiempo_mes);
 
+	/*
 	--TABLA DE HECHOS DE LAS VENTAS
 	CREATE TABLE Ventas(
 		venta_moneda numeric(18,0) FOREIGN KEY REFERENCES LOS_BORBOTONES.BI_D_Moneda(moneda_codigo) NOT NULL,
@@ -134,7 +136,12 @@ BEGIN
 		venta_cuatrimestre numeric(18,2)
 		PRIMARY KEY(venta_moneda,venta_rango_etario_inquilino,venta_anio_inicio)
 	)   */
-
+	--TABLA DE HECHOS PAGO VENTA
+	create table LOS_BORBOTONES.BI_TH_PagoAlquiler (
+		pagoalquiler_anio_pago numeric(18,0),
+		pagoalquier_mes_pago numeric(18,0),
+		pagoalquiler_porcentaje_incumplimiento numeric(18,2)
+	)
 END
 GO
 
@@ -323,6 +330,43 @@ BEGIN
         LOS_BORBOTONES.ObtenerRangoM2(inmueble_superficie),
 		anuncio_tipoOperacion,
         anuncio_moneda;
+
+	INSERT INTO LOS_BORBOTONES.BI_TH_Alquiler 
+		(alquiler_rango_etario_inquilino,
+		alquiler_barrio,
+		alquiler_anio_inicio,
+		alquiler_cuatrimestre_inicio,
+		alquiler_mes_inicio,
+		alquiler_comision)
+	select LOS_BORBOTONES.ObtenerRangoEtario(persona_fecha_nacimiento),
+		inmueble_barrio,
+		year(alqui.alquiler_fecha_inicio),
+		LOS_BORBOTONES.Cuatrimestre(alqui.alquiler_fecha_inicio),
+		month(alqui.alquiler_fecha_inicio),
+		avg(alqui.alquiler_comision)
+	from LOS_BORBOTONES.alquiler alqui
+	join LOS_BORBOTONES.inquilino on alquiler_codigo = inquilino_alquiler
+	join LOS_BORBOTONES.persona on inquilino_persona = persona_codigo
+	join LOS_BORBOTONES.anuncio on alquiler_anuncio_codigo = anuncio_codigo
+	join LOS_BORBOTONES.inmueble on inmueble_codigo = anuncio_inmueble
+		group by year(alqui.alquiler_fecha_inicio),
+	LOS_BORBOTONES.Cuatrimestre(alqui.alquiler_fecha_inicio),
+	month(alqui.alquiler_fecha_inicio),
+	LOS_BORBOTONES.ObtenerRangoEtario(persona_fecha_nacimiento),
+	inmueble_barrio
+	
+	INSERT INTO LOS_BORBOTONES.BI_TH_PagoAlquiler
+		(pagoalquiler_anio_pago,
+		pagoalquier_mes_pago,
+		pagoalquiler_porcentaje_incumplimiento )
+
+	select year(pagoAlquiler_fechaPago) as 'Año de pago',
+		month(pagoAlquiler_fechaPago) as 'Mes de pago',
+		sum(case when (datediff(day, pagoAlquiler_fechaPago, pagoAlquiler_fechaFinPeriodo) > 0) then 1 else 0 END) / COUNT(*) * 100 as 'Porcentaje Incumplimiento'
+
+	from LOS_BORBOTONES.PagoAlquiler
+	group by year(pagoAlquiler_fechaPago)
+		
 END
 
 GO
@@ -332,48 +376,57 @@ exec LOS_BORBOTONES.migracionDatosBI
 GO	
 
 --------------------------------------------------------VISTAS---------------------------------------------------------
+/*Vista 1: Duración promedio (en días) que se encuentran publicados los anuncios
+según el tipo de operación (alquiler, venta, etc), barrio y ambientes para cada
+cuatrimestre de cada año. Se consideran todos los anuncios que se dieron de alta
+en ese cuatrimestre. La duración se calcula teniendo en cuenta la fecha de alta y
+la fecha de finalización.*/
 
 CREATE VIEW LOS_BORBOTONES.VistaDuracionPromedioAnuncios
 AS
-	SELECT 
-		a.anuncio_anio_publicacion as 'Año publicacion anuncio',
+	SELECT a.anuncio_anio_publicacion as 'Año publicacion anuncio',
 		a.anuncio_cuatrimestre_publicacion as 'Cuatrimestre publicacion anuncio',
 		tipoOp.tipoOperacion_detalle 'Tipo Operacion',
 		ambi.ambientes_detalle 'Ambientes',
 		bar.barrio_detalle as 'Barrio donde se publico el anuncio',
 		avg(anuncio_duracion_en_dias) AS 'Duracion promedio en dias'
-	FROM
-		LOS_BORBOTONES.BI_TH_Anuncio a
+
+	FROM LOS_BORBOTONES.BI_TH_Anuncio a
 		JOIN LOS_BORBOTONES.BI_D_Barrio bar ON bar.barrio_codigo = a.anuncio_barrio
 		JOIN LOS_BORBOTONES.BI_D_TipoOperacion tipoOp ON a.anuncio_tipoOperacion = tipoOp.tipoOperacion_codigo
 		JOIN LOS_BORBOTONES.BI_D_ambientes ambi ON a.anuncio_ambientes = ambi.ambientes_codigo
-	GROUP BY
-		a.anuncio_anio_publicacion ,
+
+	GROUP BY a.anuncio_anio_publicacion ,
 		a.anuncio_cuatrimestre_publicacion,
 		bar.barrio_detalle,
 		ambi.ambientes_detalle,
 		tipoOp.tipoOperacion_detalle
 GO
 
+/*Vista 2: Precio promedio de los anuncios de inmuebles según el tipo de operación
+(alquiler, venta, etc), tipo de inmueble y rango m2 para cada cuatrimestre/año.
+Se consideran todos los anuncios que se dieron de alta en ese cuatrimestre. El
+precio se debe expresar en el tipo de moneda que corresponda, identificando de
+cuál se trata.*/
+
 CREATE VIEW LOS_BORBOTONES.VistaPrecioPromedioAnuncios
 AS
-SELECT 
-    anuncio_anio_publicacion as 'Año publicacion anuncio',
+SELECT anuncio_anio_publicacion as 'Año publicacion anuncio',
 	anuncio_cuatrimestre_publicacion as 'Cuatrimestre publicacion anuncio',
     tipoOperacion_detalle as 'Tipo Operacion',
     tipo_inmueble_detalle AS 'Tipo Inmueble',
     rangoM2_Inicio AS 'Rango Inicio',
     rangoM2_Fin AS 'Rango Fin',
     moneda_detalle AS 'Moneda Detalle',
-    AVG(anuncio_precioPromedio) AS 'Precio promedio'
-FROM
-    LOS_BORBOTONES.BI_TH_Anuncio 
+    avg(anuncio_precioPromedio) AS 'Precio promedio'
+
+FROM LOS_BORBOTONES.BI_TH_Anuncio 
     JOIN LOS_BORBOTONES.BI_D_TipoOperacion ON anuncio_tipoOperacion = tipoOperacion_codigo
     JOIN LOS_BORBOTONES.BI_D_TipoInmueble ON anuncio_tipo_inmueble = tipo_inmueble_codigo 
     JOIN LOS_BORBOTONES.BI_D_RangoM2 ON anuncio_rango_metros_cuadrados = rangoM2_codigo
     JOIN LOS_BORBOTONES.BI_D_Moneda ON anuncio_moneda = moneda_codigo 
-GROUP BY
-    anuncio_anio_publicacion,
+
+GROUP BY anuncio_anio_publicacion,
     anuncio_cuatrimestre_publicacion,
     tipoOperacion_detalle,
     tipo_inmueble_detalle,
@@ -382,12 +435,31 @@ GROUP BY
     moneda_detalle
 GO
 
+/*Vista 3: Los 5 barrios más elegidos para alquilar en función del rango etario de los
+inquilinos para cada cuatrimestre/año. Se calcula en función de los alquileres
+dados de alta en dicho periodo.*/
+
+CREATE VIEW LOS_BORBOTONES.VistaBarriosMasElegidosAlquilar
+AS
+SELECT TOP (5) alquiler_anio_inicio as 'Año publicacion anuncio',
+	alquiler_cuatrimestre_inicio as 'Cuatrimestre publicacion anuncio',
+    rangoEtario_Inicio as 'Rango etario inicio',
+    rangoEtario_Fin as 'Rango etario fin',
+    barrio_detalle as 'Barrio',
+    count(*) AS 'Cantidad Alquileres'
+FROM LOS_BORBOTONES.BI_TH_Alquiler
+    JOIN LOS_BORBOTONES.BI_D_RangoEtario ON rangoEtario_codigo = alquiler_rango_etario_inquilino
+    JOIN LOS_BORBOTONES.BI_D_Barrio ON barrio_codigo = alquiler_barrio
+
+GROUP BY alquiler_anio_inicio,
+    alquiler_cuatrimestre_inicio,
+    rangoEtario_Inicio,
+    rangoEtario_Fin,
+    barrio_detalle
+ORDER BY count(*) DESC
+GO
 
 
-
-
-
-
----------------------------INICIO SCRIPT script_creacion_BI---------------------
+---------------------------fIN SCRIPT script_creacion_BI---------------------
 
 
